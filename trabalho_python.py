a93dd610447446ae691e16209c6b55abed4e61cd
estevao.pereira <estevaopp05@gmail.com>
2024-08-23 08:21:54 -0300

    tabalho python feito

---
+++ trabalho_python.py
@@ -0,0 +1,10 @@
+T = [-10, -5, 0, 1, 3, 5, -6, -8]
+
+menor_temperatura = min(T)
+maior_temperatura = max(T)
+
+media_temperatura = sum(T) / len(T)
+
+print(f"A menor temperatura é: {menor_temperatura}°C")
+print(f"A maior temperatura é: {maior_temperatura}°C")
+print(f"A temperatura média é: {media_temperatura:.2f}°C")
